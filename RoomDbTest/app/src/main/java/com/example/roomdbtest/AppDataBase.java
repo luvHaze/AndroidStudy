962bc9ea89748ed3a755e3ba1231d6aa5d0401f5
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-02-22 20:23:50 +0900

    Room Database insert

---
+++ RoomDbTest/app/src/main/java/com/example/roomdbtest/AppDataBase.java
@@ -0,0 +1,12 @@
+package com.example.roomdbtest;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Todo.class}, version=1)
+public abstract class AppDataBase extends RoomDatabase {
+
+    public abstract TodoDao todoDao();
+
+
+}
