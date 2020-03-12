74be9a5294f427d826f954c6cf90554de9c817da
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-03-12 21:13:19 +0900

    유저 기능 추가 follow 기능 강화 (follow 버튼 클릭 시 follower 추가) FollowDTO 추가

--- Exam_instagram/app/src/main/java/com/example/exam_instagram/MainActivity.kt
+++ Exam_instagram/app/src/main/java/com/example/exam_instagram/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.exam_instagram
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +14,12 @@ import com.example.exam_instagram.navigation.AlarmFragment
 import com.example.exam_instagram.navigation.DetailViewFragment
 import com.example.exam_instagram.navigation.GridFragment
 import com.example.exam_instagram.navigation.UserFragment
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.jar.Manifest
 
@@ -38,9 +43,13 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.add_photo -> {
 
-                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED){
-                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
                 }
 
                 return true
@@ -53,10 +62,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
-                var bundle=Bundle()
+                var bundle = Bundle()
                 var uid = FirebaseAuth.getInstance().currentUser?.uid
-                bundle.putString("destinationUid",uid)
-                userFragment.arguments=bundle
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.mainContent, userFragment).commit()
                 return true
@@ -65,10 +74,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         return false
     }
 
-    fun setToolbarDefault(){
-        toolbar_username.visibility= View.GONE
-        btn_toolbar_back.visibility=View.GONE
-        toolbar_title_image.visibility=View.VISIBLE
+    fun setToolbarDefault() {
+        toolbar_username.visibility = View.GONE
+        btn_toolbar_back.visibility = View.GONE
+        toolbar_title_image.visibility = View.VISIBLE
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,10 +85,33 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
 
-        ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
 
         //Set default Screen
-        bottom_navigation.selectedItemId=R.id.action_home
+        bottom_navigation.selectedItemId = R.id.action_home
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == UserFragment.PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK) {
+            var imageUri = data?.data
+            var uid = FirebaseAuth.getInstance().currentUser?.uid
+            var storageRef =
+                FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid!!)
+            storageRef.putFile(imageUri!!).continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+                return@continueWithTask storageRef.downloadUrl
+            }.addOnSuccessListener { uri ->
+                var map = HashMap<String, Any>()
+                map["image"] = uri.toString()
+                FirebaseFirestore.getInstance().collection("profileImages").document(uid).set(map)
+            }
+
+        }
     }
 
 
