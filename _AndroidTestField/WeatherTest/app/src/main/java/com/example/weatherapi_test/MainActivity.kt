7f29cb12d1cf410fe6937aa53f6ad8eb72194f1c
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-03-19 22:59:47 +0900

    20200319 Study

--- _AndroidTestField/WeatherTest/app/src/main/java/com/example/weatherapi_test/MainActivity.kt
+++ _AndroidTestField/WeatherTest/app/src/main/java/com/example/weatherapi_test/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.weatherapi_test
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val API_KEY = "ef7108872ae71fd5fd237a0cdd318096"
+        val DEFAULT_API = "api.openweathermap.org/data/2.5/weather"
 
-        URL
-        
+        var uri :Uri = Uri.parse("api.openweathermap.org/data/2.5/forecast?id=524901").buildUpon()
+                .appendQueryParameter("APPID",API_KEY).build()
+
+        GlobalScope.launch {
+            weatherAPI_Call()
+        }
+    }
+
+
+    fun weatherAPI_Call (){
 
     }
 }
