345bc2c18851771221466739f8ea87ae34c37f50
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-04-03 16:48:03 +0900

    Survival_pr3

--- _Survival_pr3/app/src/main/java/luv/zoey/survival_pr3/MainActivity.kt
+++ _Survival_pr3/app/src/main/java/luv/zoey/survival_pr3/MainActivity.kt
@@ -1,10 +1,17 @@
 package luv.zoey.survival_pr3
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,20 +29,93 @@ class MainActivity : AppCompatActivity() {
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 webView.loadUrl(urlEditText.text.toString())
                 true
-            }
-            else {
+            } else {
                 false
             }
 
         }
         webView.loadUrl("http://www.google.com")
+
+        registerForContextMenu(webView)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+
+            R.id.action_naver -> {
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("http://www.daum.net")
+                return true
+            }
+
+            R.id.action_call -> {
+                //암시적인텐트
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:032-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                sendSMS("031-123-4567",webView.url)
+                return true
+            }
+
+            R.id.action_email -> {
+                email("test@excample.com","whgdmstkdlxm",webView.url)
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context,menu)
+    }
+
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+
+            R.id.action_share ->{
+                share(webView.url)
+
+            }
+
+            R.id.action_browser -> {
+                browse(webView.url)
+            }
+        }
+
+        return super.onContextItemSelected(item)
+    }
+
 
     override fun onBackPressed() {
 
-        if(webView.canGoBack()){
+        if (webView.canGoBack()) {
             webView.goBack()
-        }else{
+        } else {
             super.onBackPressed()
         }
 
