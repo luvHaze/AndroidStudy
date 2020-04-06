4b2e3ed50e64210cc1cef369a229b7e4533beaea
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-04-06 22:03:12 +0900

    Survival_pr5

---
+++ Survival_pr5/app/src/main/java/luv/zoey/survival_pr5/MainActivity.kt
@@ -0,0 +1,122 @@
+package luv.zoey.survival_pr5
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
+import java.util.jar.Manifest
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+
+                alert(
+                    "사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다.",
+                    "권한이 필요한 이유"
+                ) {
+                    yesButton {
+
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE
+                        )
+                    }
+                    noButton { }
+
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_READ_EXTERNAL_STORAGE
+                )
+            }
+        } else {
+            getAllPhotos()
+        }
+
+    }
+
+    private fun getAllPhotos() {
+        // 모든 사진 정보 가져오기
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,// 가져올 항목 배열
+            null, // 조건
+            null, // 조건
+            MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC"
+        )
+        var fragments = ArrayList<Fragment>()
+        if(cursor!=null){
+            while(cursor.moveToNext()){
+                //사진 경로 Uri 가져오기
+                val uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d("MainActivity",uri)
+                fragments.add(PhotoFragment.newInstance(uri))
+            }
+            cursor.close()
+            //커서 객체는 사용하지 않을때는 close() 메서드로 닫아야 한다. (메모리 누수 방지)
+        }
+
+        val adapter=MyPagerAdapter(supportFragmentManager)
+        adapter.updateFragments(fragments)
+        viewPager.adapter=adapter
+
+        //타이머 기능
+        /*timer(period=3000){
+            runOnUiThread {
+                if(viewPager.currentItem<adapter.count-1){
+                    viewPager.currentItem=viewPager.currentItem+1
+                }else{
+                    viewPager.currentItem=0
+                }
+            }
+        }*/
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    getAllPhotos()
+                } else {
+                    toast("권한 거부됨")
+                }
+                return
+            }
+
+        }
+    }
+}
