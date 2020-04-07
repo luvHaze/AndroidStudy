5374c568d31d822e636d96a9db3d58e8d3423366
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-04-07 22:37:30 +0900

    edWith_pr1

--- edWithPR/app/src/main/java/luv/zoey/edwith_pr/MainActivity.kt
+++ edWithPR/app/src/main/java/luv/zoey/edwith_pr/MainActivity.kt
@@ -2,11 +2,14 @@ package luv.zoey.edwith_pr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
