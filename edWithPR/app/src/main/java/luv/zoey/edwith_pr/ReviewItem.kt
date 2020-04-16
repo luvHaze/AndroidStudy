6eb955153b015842790ff3c4b526a76bfc3eae2b
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-04-16 19:45:01 +0900

    edWith_pr3 (Problem Complete) 레이팅바 , 모두보기 구현 모두보기 구현에서는 Parcelable을 사용했다.

--- edWithPR/app/src/main/java/luv/zoey/edwith_pr/ReviewItem.kt
+++ edWithPR/app/src/main/java/luv/zoey/edwith_pr/ReviewItem.kt
@@ -4,6 +4,10 @@ import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+
+//액티비티간의 객체 전달을 위해선
+//Parcelize를 해서 Parcelable 하게 만들어 주어야
+//get/put ParcelableArrayListExtra 함수를 사용할 수 있게 된다.
 @Parcelize
 data class ReviewItem(
     var content : String,
--- edWithPR/app/src/main/java/luv/zoey/edwith_pr/ReviewReadActivity.kt
+++ edWithPR/app/src/main/java/luv/zoey/edwith_pr/ReviewReadActivity.kt
@@ -1,5 +1,6 @@
 package luv.zoey.edwith_pr
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
@@ -17,13 +18,17 @@ class ReviewReadActivity : AppCompatActivity() {
         setContentView(R.layout.activity_review_read)
         setSupportActionBar(findViewById(R.id.toolbar2))
 
-        actionBar?.setHomeButtonEnabled(true)
+        actionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE)
 
+        // Main에서 받은 인텐트에서 데이터를 추출해 내고
         dataList= intent.extras?.getParcelableArrayList<ReviewItem>("content") as ArrayList<ReviewItem>
 
+
+        //어뎁터 설정을 한 뒤 내보내 준다.
         val layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         readReviewAll_RecyclerView.layoutManager=layoutManager
 
+        // 어뎁터 설정
         readReviewAll_RecyclerView.adapter=ReviewAdapter(dataList)
     }
 }
