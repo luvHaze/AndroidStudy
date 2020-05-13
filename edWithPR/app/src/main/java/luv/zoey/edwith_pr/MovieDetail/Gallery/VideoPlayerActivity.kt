ec83764d1df7daf21cd9aea2e1a9db26d1a9e890
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-05-13 22:52:20 +0900

    edWith_pr7                                                                  (Volley를 이용해 API 통신 성공 , API 요청 -> 응답,                                                                   응답받은 데이터로 프래그먼트 생성 성공,                                                                   이미지 AsyncTask 이용해서 이미지 불러오기 성공                                                                    * Glide 이용해서 이미지 불러오기 성공                                                                      -> Glide 이용이 딜레이가 없어서 좋은거 같음                                                                         AsyncTask는 주석처리함
                                                                       * Detail 쪽 응답 받고 뷰에 값들 설정함
    
                                                                       * Detail 리뷰 모음 가져왔음
    
                                                                       * 리뷰 READ / WRITE 모두 데이터 타입에 맞춰서 수정함
    
                                                                       * API 서버에 한풀평 작성, 추천하기 완료함
    
                                                                       * API 서버에 좋아요&싫어요 버튼 & 구현함
    
                                                                       * Room을 이용해 데이터베이스 구현중 (DAO, Entity,
                                                                         DataBase) 만들었고 이제 연동하는 과정 중
    
                                                                       * Room 말고 Realm 사용해서 데이터베이스 비었을 때만 네트워크 사용 데이터베이스에 파일이 존재하면 파일을 사용하도록 했다.
    
                                                                       * MovieDetail 모델을 RealmObject 추가시 에러 발생 -> Realm 구조 이해 필요
    
                                                                       * 에러 발생 원인은 Realm은 모델이 추가되거나 모델의 구조가 변경될때 마이그레이션이 필요하다 마이그레이션없이 진행하려면 오류가 뜬다 그래서 개발할 때에는 Realm 객체를 생성할때 config(설정) 을 마이그레이션이 필요할 시 알아서 삭제해 주는 설정을 해 둔다.
    
                                                                       * DB 추가 완료 (메인메뉴, 영화 상세화면, 리뷰)
    
                                                                       * 사진, 동영상 보기 기능 추가진행중 ( Glide, ExoPlayer 사용)
    
                                                                       * ExoPlayer Youtube 주소로 재생 가능하게끔 해야 한다.
    
                                                                       * 재생 버튼 없는 이미지 클릭 시 -> ImageViewActivity로
                                                                       * 재생 버튼 있는 이미지 클릭 시 -> VideoPlayerActivity로 이동 intent 에는 URL 첨부
       *  코틀린 코드에선 화면 전환시 화면이 초기화 되는 현상이 발생

--- edWithPR/app/src/main/java/luv/zoey/edwith_pr/MovieDetail/Gallery/VideoPlayerActivity.kt
+++ edWithPR/app/src/main/java/luv/zoey/edwith_pr/MovieDetail/Gallery/VideoPlayerActivity.kt
@@ -1,36 +1,111 @@
 package luv.zoey.edwith_pr.MovieDetail.Gallery
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import luv.zoey.edwith_pr.R
 
 class VideoPlayerActivity : AppCompatActivity() {
+
+    private var player : SimpleExoPlayer? = null
+    private lateinit var playerView : PlayerView
+    private var playWhenReady = true
+    private var currentWindow : Int  = 0
+    private var playbackPosition : Long = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
 
-        Toast.makeText(this,intent.extras?.getString("URL").toString(), Toast.LENGTH_LONG).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
+        playerView = findViewById(R.id.playerView)
     }
 
     override fun onResume() {
         super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23 || player==null) {
+            initPlayer()
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
+        if(Util.SDK_INT>23){
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
     }
 
     override fun onStop() {
         super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
     }
 
-    //TODO
-    //ExoPlayer Youtube 주소로 재생 가능하게끔 하기
+
+
+    private fun initPlayer(){
+        //TODO
+
+        player = ExoPlayerFactory.newSimpleInstance(this)
+        playerView.player = player
+
+        var uri = Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+        var mediaSource = buildMediaSource(uri)
+
+        player?.playWhenReady=playWhenReady
+        player?.seekTo(currentWindow,playbackPosition)
+        player?.prepare(mediaSource,false,false)
+
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            playWhenReady = player!!.playWhenReady
+            player!!.release()
+            player = null
+        }
+    }
+
+    private fun buildMediaSource(uri : Uri) : MediaSource
+    {
+        val dataSourceFactory: DataSource.Factory =
+            DefaultDataSourceFactory(this, "exoplayer-codelab")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(uri)
+    }
+
+
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                )
+    }
+
 }
